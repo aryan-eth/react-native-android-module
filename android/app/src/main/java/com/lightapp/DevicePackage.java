353d15bcc76ce06b12d7ace5e5d797d16e5d9384
Aryan Singh <aryansingh@umass.edu>
2019-09-26 12:00:02 -0400

    added modules to mainApplication

--- android/app/src/main/java/com/lightapp/DevicePackage.java
+++ android/app/src/main/java/com/lightapp/DevicePackage.java
@@ -1,6 +1,7 @@
 package com.lightapp;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.annotations.ReactModule;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+//This file just imports our module and instantiates it.
 public class DevicePackage implements ReactPackage {
     @Nonnull
     @Override
@@ -20,7 +22,7 @@ public class DevicePackage implements ReactPackage {
         List<NativeModule> list = new ArrayList<>();
         // add different Native Modules to the list and then return the list
         list.add(new DeviceModule(reactContext));
-        return null;
+        return list;
     }
 
     @Nonnull
@@ -28,4 +30,14 @@ public class DevicePackage implements ReactPackage {
     public List<ViewManager> createViewManagers(@Nonnull ReactApplicationContext reactContext) {
         return null;
     }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules(){
+        return new ArrayList<>();
+    }
 }
+
+// This is the index.js or app.js code - we can access this module there
+//import {NativeModules} from 'react-native';
+//        NativeModules.Device.getDeviceName((err ,name) => {
+//        console.log(err, name);
+//        });
--- android/app/src/main/java/com/lightapp/MainApplication.java
+++ android/app/src/main/java/com/lightapp/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.lightapp.DevicePackage;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,13 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-//            List<ReactPackage> packages = new PackageList(this).getPackages();
-//       Packages that cannot be autolinked yet can be added manually here, for example:
-//       packages.add(new MyReactNativePackage());
-//      return packages;
+      // We have to add all the packages here
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
-              new BulbPackage()
+              new BulbPackage(),
+              new DevicePackage()
       );
     }
 
